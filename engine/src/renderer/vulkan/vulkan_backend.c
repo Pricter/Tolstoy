2cf2afd97bc791898822749a63a039f21c2d063e
BuildTools <unconfigured@null.spigotmc.org>
2023-03-09 17:46:15 +0530

    Vulkan Physical Device

--- engine/src/renderer/vulkan/vulkan_backend.c
+++ engine/src/renderer/vulkan/vulkan_backend.c
@@ -1,6 +1,7 @@
 #include "vulkan_backend.h"
-
 #include "vulkan_types.inl"
+#include "vulkan_device.h"
+
 #include "core/logger.h"
 
 #include "core/tstring.h"
@@ -103,6 +104,18 @@ b8 vulkan_renderer_backend_initialize(renderer_backend* backend, const char* app
     VK_CHECK(func(context.instance, &debug_create_info, context.allocator, &context.debug_messenger));
     TDEBUG("Vulkan debugger created.");
 #endif
+
+    TDEBUG("Creating Vulkan surface...");
+    if(!platform_create_vulkan_surface(plat_state, &context)) {
+        TERROR("Failed to create platform surface!");
+        return FALSE;
+    }
+    TDEBUG("Vulkan surface created.");
+
+    if(!vulkan_device_create(&context)) {
+        TERROR("Failed to create device!");
+        return FALSE;
+    }
 
     TINFO("Vulkan renderer initialized successfully.");
     return TRUE;
