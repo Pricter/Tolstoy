d8ce77ac757ed98b731c20c0fa26474790884c69
BuildTools <unconfigured@null.spigotmc.org>
2023-03-09 11:23:19 +0530

    Implement delta time in application and start work on renderer

--- engine/src/core/application.c
+++ engine/src/core/application.c
@@ -5,6 +5,7 @@
 #include "core/tmemory.h"
 #include "core/event.h"
 #include "core/input.h"
+#include "core/clock.h"
 
 typedef struct application_state {
     game* game_inst;
@@ -13,6 +14,7 @@ typedef struct application_state {
     platform_state platform;
     i16 width;
     i16 height;
+    clock clock;
     f64 last_time;
 } application_state;
 
@@ -69,25 +71,58 @@ b8 application_create(game* game_inst) {
 }
 
 b8 application_run() {
+    clock_start(&app_state.clock);
+    clock_update(&app_state.clock);
+
+    app_state.last_time = app_state.clock.elapsed;
+
+    // f64 running_time = 0;
+    // u8 frame_count = 0;
+    f64 target_frame_seconds = 1.0f / 60;
+
     TINFO(get_memory_usage_str());
+
     while(app_state.is_running) {
         if(!platform_pump_messages(&app_state.platform)) {
             app_state.is_running = FALSE;
         }
         if(!app_state.is_suspended) {
-            if(!app_state.game_inst->update(app_state.game_inst, (f32)0)) {
+            clock_update(&app_state.clock);
+            f64 current_time = app_state.clock.elapsed;
+            f64 delta = (current_time - app_state.last_time);
+            f64 frame_start_time = platform_get_absolute_time();
+
+            if(!app_state.game_inst->update(app_state.game_inst, (f32)delta)) {
                 TFATAL("Game update failed, shutting down.");
                 app_state.is_running = FALSE;
                 break;
             }
 
-            if(!app_state.game_inst->render(app_state.game_inst, (f32)0)) {
+            if(!app_state.game_inst->render(app_state.game_inst, (f32)delta)) {
                 TFATAL("Game render failed, shutting down.");
                 app_state.is_running = FALSE;
                 break;
             }
 
-            input_update(0);
+            f64 frame_end_time = platform_get_absolute_time();
+            f64 frame_elapsed_time = frame_end_time - frame_start_time;
+            // running_time += frame_elapsed_time;
+            f64 remaining_seconds = target_frame_seconds - frame_elapsed_time;
+
+            if(remaining_seconds > 0) {
+                u64 remaining_ms = (remaining_seconds * 1000);
+
+                b8 limit_frames = FALSE;
+                if(remaining_ms > 0 && limit_frames) {
+                    platform_sleep(remaining_ms - 1);
+                }
+
+                // frame_count++;
+            }
+
+            input_update(delta);
+
+            app_state.last_time = current_time;
         }
     }
     app_state.is_running = FALSE;
